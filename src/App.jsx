f7f7ea2025bfb77256e85660843c9010a07eb7cb
Victoria-Sampalo <victoriasampalo19@gmail.com>
2024-07-10 14:21:46 +0200

    en construccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+     <h2>¡Página en construcción!</h2> 
+
+<p>¡Hola! Estamos trabajando duro para crear una experiencia web increíble para ti.</p>
+
+<p>En este momento, la página se encuentra en construcción, pero volveremos pronto con algo mejor que nunca.</p>
       
     </>
   )
